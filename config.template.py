db0078a7c20b72cb09c9d58590988db0af71bd2f
PhalHuon <huonchamroeunphal1029@gmail.com>
2025-05-10 01:30:03 -0400

    fourth commit

---
+++ config.template.py
@@ -0,0 +1,10 @@
+CONFIG = {
+    'database': {
+        'host': 'db.your-supabase-host.supabase.co',
+        'port': '5432',
+        'user': 'postgres',
+        'password': 'your-password',
+        'database': 'postgres',
+        'connection_string': 'postgresql://postgres:your-password@db.your-supabase-host.supabase.co:5432/postgres'
+    }
+}
